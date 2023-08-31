423f2f49f137c6e0b78d7575e36311c06435349d
xxkinaxx <ekopandu088@gmail.com>
2023-08-31 14:39:25 +0700

    My first project

---
+++ asset/js/index.js
@@ -0,0 +1,7 @@
+// navbar toggle button
+const toggleButton = document.getElementsByClassName('toggle')[0];
+const navbarItem =document.getElementsByClassName('navbar-item');
+toggleButton.addEventListener('click', function(){
+    for(let i = 0; i < navbarItem.length; i++)
+        navbarItem[i].classList.toggle('active')
+});
